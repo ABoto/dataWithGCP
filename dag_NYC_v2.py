e0c38c81f1dd7950273a5d3cc95bab820ec97fdb
ABoto <batabyal.arnab@gmail.com>
2024-10-23 03:08:35 +0530

    Add files via upload

--- dag_NYC_v2.py
+++ dag_NYC_v2.py
@@ -7,6 +7,25 @@ import os
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/arnab/WorkSpace/integrated-hawk-433114-s2-f604d4f40e6f.json"
 
+#### DAG with create, and delete cluster ####
+#cluster_name = "cluster-8399-testboto"
+region = "us-central1"
+project_id = "integrated-hawk-433114-s2"
+bucket_name = 'spark_jobs_test_boto'
+cluster_name = 'cluster_for_test_boto'
+pySpark_clean_URI = "gs://spark_jobs_test_boto/script/data_clean_NYC.py"
+pySpark_insert_URI = "gs://spark_jobs_test_boto/script/data_Ingest2bq_NYC.py"
+
+cluster_config = ClusterGenerator(
+    project_id=project_id,
+    zone="us-central1-a",
+    master_machine_type="n1-standard-2",
+    worker_machine_type="n1-standard-2",
+    num_workers=2,
+    worker_disk_size=50,
+    master_disk_size=100,
+    storage_bucket=BUCKET_NAME,
+).make()
 
 default_args = {
     'owner': 'airflow',
@@ -25,12 +44,6 @@ dag = DAG('fetch_NYC_data',
           schedule_interval='@daily',
           catchup=False)
 
-# Dataproc cluster details
-cluster_name = "cluster-8399-testboto"
-region = "us-central1"
-project_id = "integrated-hawk-433114-s2"
-pySpark_clean_URI = "gs://spark_jobs_test_boto/script/data_clean_NYC.py"
-pySpark_insert_URI = "gs://spark_jobs_test_boto/script/data_Ingest2bq_NYC.py"
 
 pySpark_job_clean = {
     "reference": {"project_id": project_id},
@@ -45,11 +58,25 @@ pySpark_job_insert = {
 }
 
 with dag:
+
+    start_pipeline = DummyOperator(
+        task_id = 'start_pipeline',
+        dag = dag
+        )
+
     run_script_task = BashOperator(
         task_id='run_script',
         bash_command='python3 /home/arnab/airflow/dags/data_extract_NYC.py',
     )
     
+    create_cluster_task = DataprocCreateClusterOperator(
+        task_id="create_cluster_task",
+        project_id=project_id,
+        cluster_config=cluster_config,
+        region=region,
+        cluster_name=cluster_name,
+    )
+
     clean_data_task = DataprocSubmitJobOperator(
         task_id="clean_data_task", 
         job=pySpark_job_clean, 
@@ -64,6 +91,18 @@ with dag:
         project_id=project_id
     )
 
-# dependencies between the tasks
+    delete_cluster = DataprocDeleteClusterOperator(
+        task_id="delete_cluster", 
+        project_id=project_id, 
+        cluster_name=cluster_name, 
+        region=region,
+    )
 
-run_script_task >> clean_data_task >> insert_data_task
+    finish_pipeline = DummyOperator(
+        task_id = 'finish_pipeline',
+        dag = dag
+    )
+
+# dependencies
+
+start_pipeline >> run_script_task >> create_cluster_task >> clean_data_task >> insert_data_task >> delete_cluster >> finish_pipeline
--- data_clean_NYC.py
+++ data_clean_NYC.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from google.cloud import storage
+from pyspark.sql.window import Window
 
 bucket_name = "spark_jobs_test_boto"
 input_file_path = "data/yellow_tripdata_2024-01.parquet"
@@ -13,6 +14,19 @@ df = df.dropDuplicates()
 df = df.filter(df["tolls_amount"] > 0)
 #agg_df = df.groupBy(df.VendorID).agg(avg(df.total_amount))
 #agg_df.show()
+# df.createOrReplaceTempView("trip_table")
+# result_df = spark.sql()
+timeFormat = "yyyy-MM-dd HH:mm:ss"
+timeDiff = (unix_timestamp('tpep_dropoff_datetime', format=timeFormat)
+            - unix_timestamp('tpep_pickup_datetime', format=timeFormat))
+df = df.withColumn("trip_duration", timeDiff)
+
+windowPartitionAgg  = Window.partitionBy("trip_Duration")
+df = df.withColumn("Avg_distance", avg(col("trip_distance")).over(windowPartitionAgg)) \
+       .withColumn("total_distance", sum(col("trip_distance")).over(windowPartitionAgg)) \
+       .withColumn("Min_distance",min(col("trip_distance")).over(windowPartitionAgg)) \
+       .withColumn("Max_distance", max(col("trip_distance")).over(windowPartitionAgg))
+       
 df.write.format("csv").option("header", "true").mode("overwrite").save(f"gs://{bucket_name}/{output_file_path}")
 #df.write.format("csv").option("header", "true").mode("overwrite").save(f"gs://{bucket_name}/{output_file_path}")
 spark.stop()
