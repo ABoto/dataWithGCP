70c153ac80460881e5822aa60d3bd2b364fd00a0
ABoto <batabyal.arnab@gmail.com>
2024-10-23 03:31:08 +0530

    Add files via upload

--- dag_NYC_v2.py
+++ dag_NYC_v2.py
@@ -1,8 +1,15 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from airflow.providers.google.cloud.operators.dataproc import DataprocSubmitJobOperator
 from airflow.utils.dates import days_ago
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.providers.google.cloud.operators.dataproc import (
+    DataprocCreateClusterOperator,
+    DataprocDeleteClusterOperator,
+    DataprocSubmitJobOperator,
+    ClusterGenerator
+)
+from airflow.providers.google.cloud.sensors.dataproc import DataprocJobSensor
 import os
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/arnab/WorkSpace/integrated-hawk-433114-s2-f604d4f40e6f.json"
@@ -12,19 +19,19 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/arnab/WorkSpace/integrated
 region = "us-central1"
 project_id = "integrated-hawk-433114-s2"
 bucket_name = 'spark_jobs_test_boto'
-cluster_name = 'cluster_for_test_boto'
+cluster_name = 'cluster-for-test-boto'
 pySpark_clean_URI = "gs://spark_jobs_test_boto/script/data_clean_NYC.py"
 pySpark_insert_URI = "gs://spark_jobs_test_boto/script/data_Ingest2bq_NYC.py"
 
 cluster_config = ClusterGenerator(
     project_id=project_id,
     zone="us-central1-a",
-    master_machine_type="n1-standard-2",
-    worker_machine_type="n1-standard-2",
+    master_machine_type="e2-standard-2",
+    worker_machine_type="e2-standard-2",
     num_workers=2,
     worker_disk_size=50,
     master_disk_size=100,
-    storage_bucket=BUCKET_NAME,
+    storage_bucket=bucket_name,
 ).make()
 
 default_args = {
@@ -38,7 +45,7 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG('fetch_NYC_data',
+dag = DAG('fetch_NYC_data_v2',
           default_args=default_args,
           description='Runs an external Python script to get Taxi data',
           schedule_interval='@daily',
